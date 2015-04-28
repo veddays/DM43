1661a3646257283c68bc98a6a7a79c339a8024cf
elimia <elimias@yandex.ru>
2015-04-28 22:07:45 +0400

    Update DEG_P_N.c

---
+++ Полиномы/DEG_P_N.c
@@ -0,0 +1,10 @@
+//Подружаемые модули отсутствуют
+//Прототип: size_t DEG_P_N (struct POLYNOMIAL st); st - структура с массивом коэффициентов factors и старшей степенью degree
+//Возвращает степень многочлена по данному набору коэффициентов
+//Необходимо подключение malloc.h
+//Мясоедова, гр.4306
+
+size_t DEG_P_N (struct POLYNOMIAL st)
+  {
+    return (_msize(st.factors)/sizeof(st.factors)-1); 
+  }
