06028e190f4f7f5a96772b5ae3aade2bc6d81fb0
DG23 <doter96@mail.ru>
2015-05-01 14:11:12 +0300

    Update ADD_1N_N.c

--- Натуральные Числа/ADD_1N_N.c
+++ Натуральные Числа/ADD_1N_N.c
@@ -7,27 +7,27 @@
 
 void ADD_1N_N(struct NATURAL* natural)
 {
-	int i, flag = 0; // flag - проверка на успешность выполнения операции
-	printf("n is %d", (*natural).index);
-	if((*natural).index > 0) // проверка на случай, было передано неправильный номер старшего разряда
-	{
-		for(i=0; i<((*natural).index) && !flag; i++)
-			if((*natural).number[i] == 9) // проверка, нужно ли делать перенос
-			{
-				(*natural).number[i] = 0;
-				if(i == (*natural).index - 1) // является ли текущая цифра последней при проверке
-				{
-					(*natural).number[(*natural).index] = 1;
-					((*natural).index)++;  // увеличиваем кол-во разрядов на 1
-					flag = 1;
-				}   
-			}
-			else
-			{
-				(*natural).number[i] = (*natural).number[i] + 1; // единица успешно добавлена
-				flag = 1;
-			}
-	}
-	else
-		puts("\nError!");
+   int i, flag = 0; // flag - проверка на успешность выполнения операции
+   printf("n is %d", (*natural).index);
+   if((*natural).index > 0) // проверка на случай, было передано неправильный номер старшего разряда
+     {
+        for(i=0; i<((*natural).index) && !flag; i++)
+	if((*natural).number[i] == 9) // проверка, нужно ли делать перенос
+	  {
+	     (*natural).number[i] = 0;
+	     if(i == (*natural).index - 1) // является ли текущая цифра последней при проверке
+	       {
+	          (*natural).number[(*natural).index] = 1;
+	          ((*natural).index)++;  // увеличиваем кол-во разрядов на 1
+	          flag = 1;
+	       }   
+	  }
+        else
+	  {
+	     (*natural).number[i] = (*natural).number[i] + 1; // единица успешно добавлена
+	     flag = 1;
+	  }
+        }
+      else
+      puts("\nError!");
 }
