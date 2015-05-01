9c64b5ae8793e88a2a5c990336ff404df1e12237
DG23 <doter96@mail.ru>
2015-05-01 16:31:56 +0300

    Update ADD_1N_N.c

--- Натуральные Числа/ADD_1N_N.c
+++ Натуральные Числа/ADD_1N_N.c
@@ -8,25 +8,20 @@
 void ADD_1N_N(struct NATURAL* natural)
 {
    int i, flag = 0; // flag - проверка на успешность выполнения операции
-   if((*natural).index > 0) // проверка на случай, было передано неправильный номер старшего разряда
-     {
-        for(i=0; i<((*natural).index) && !flag; i++)
-	if((*natural).number[i] == 9) // проверка, нужно ли делать перенос
-	  {
-	     (*natural).number[i] = 0;
-	     if(i == (*natural).index - 1) // является ли текущая цифра последней при проверке
-	       {
-	          (*natural).number[(*natural).index] = 1;
-	          ((*natural).index)++;  // увеличиваем кол-во разрядов на 1
-	          flag = 1;
-	       }   
-	  }
-        else
-	  {
-	     (*natural).number[i] = (*natural).number[i] + 1; // единица успешно добавлена
-	     flag = 1;
-	  }
+   for(i=0; i<((*natural).index) && !flag; i++)
+   if((*natural).number[i] == 9) // проверка, нужно ли делать перенос
+      {
+         (*natural).number[i] = 0;
+	 if(i == (*natural).index - 1) // является ли текущая цифра последней при проверке
+	   {
+	      (*natural).number[(*natural).index] = 1;
+	      ((*natural).index)++;  // увеличиваем кол-во разрядов на 1
+	      flag = 1;
+	   }   
       }
     else
-    puts("\nError!");
+      {
+         (*natural).number[i] = (*natural).number[i] + 1; // единица успешно добавлена
+	 flag = 1;
+      }
 }
